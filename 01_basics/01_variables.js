b3b21acd8e5008eb51460cd64ac3ffadabe73d98
AfzalMusliyar <afzal.musliyar24@spit.ac.in>
2025-07-28 14:32:28 +0000

    changed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=46512
+let accountEmail="afzalmuxliyar@gmail.com"
+var accountPassword="abcd"
+accountCity="jaipur"
+let accountState
+
+//accountId=46512   //gives error for changing constant thing
+
+accountEmail="afzalmuxliyar@gmail.com"
+accountPassword="abcd"
+accountCity="jaipur"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var
+as it gives scope issues
+*/
