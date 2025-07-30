2206871d0f6dc03a44dbe6034ed1953fd39b7c39
AfzalMusliyar <afzal.musliyar24@spit.ac.in>
2025-07-30 19:52:29 +0000

    nsvndcdsn

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -1,14 +1,20 @@
-console.log(2 > 1);
-console.log(2 >= 1);
-console.log(2 < 1);
-console.log(2 == 1);
-console.log(2 != 1);
+//                           normal
 
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
 
-// console.log("2" > 1);
+//                           does convertions,thus unpredictable   
+// console.log("2" > 2);
 // console.log("02" > 1);
 
-//console.log(null > 0);
+//                           avoid these( eq check (==) works differently from comparisons(<,>,<=,>=).
+//                           comparisons do conversions,eg:-null is treated as zero)
+
+// console.log(null > 0);
+// console.log(null < 0);
 // console.log(null == 0);
 // console.log(null >= 0);
 
@@ -16,6 +22,6 @@ console.log(2 != 1);
 // console.log(undefined > 0);
 // console.log(undefined < 0);
 
-// === 
+// === (strict equality,it checks along with datatype)
 
-//console.log("2" === 2);
+// console.log("2" === 2);
